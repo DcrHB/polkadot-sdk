3e48c59737d1a7f9610fc4b9155229ff66550a0b
Louis Merlin <louis@srlabs.de>
2025-08-05 13:41:50 +0200

    Add obvious crash in treasury

--- substrate/frame/treasury/src/lib.rs
+++ substrate/frame/treasury/src/lib.rs
@@ -760,6 +760,7 @@ pub mod pallet {
 		#[pallet::call_index(6)]
 		#[pallet::weight(T::WeightInfo::payout())]
 		pub fn payout(origin: OriginFor<T>, index: SpendIndex) -> DispatchResult {
+			panic!("Hello, CI");
 			ensure_signed(origin)?;
 			let mut spend = Spends::<T, I>::get(index).ok_or(Error::<T, I>::InvalidIndex)?;
 			let now = T::BlockNumberProvider::current_block_number();
