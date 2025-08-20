e694ae5e834c51bafcf95ef81cdef66d41b2dbed
Louis Merlin <louis@srlabs.de>
2025-08-20 08:56:55 +0200

    Remove thing

--- substrate/frame/treasury/src/lib.rs
+++ substrate/frame/treasury/src/lib.rs
@@ -689,8 +689,6 @@ pub mod pallet {
 			let expire_at = valid_from.saturating_add(T::PayoutPeriod::get());
 			ensure!(expire_at > now, Error::<T, I>::SpendExpired);
 
-			panic!("This shouldn't be happening");
-
 			let native_amount =
 				T::BalanceConverter::from_asset_balance(amount, *asset_kind.clone())
 					.map_err(|_| Error::<T, I>::FailedToConvertBalance)?;
